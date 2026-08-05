0ae98c3b3d2a4614d9f98696391e0edb6429a6aa
simon08080808 <komsimon059@gmail.com>
2026-08-05 21:35:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-const net = require('net');
+const http = require('http');
 
 const TARGET_HOST = 'simon.benbilal237free.xyz';
 const TARGET_PORT = 80;
 const PORT = process.env.PORT || 8080;
 
-const server = net.createServer((clientSocket) => {
-  const targetSocket = net.connect(TARGET_PORT, TARGET_HOST, () => {
-    clientSocket.pipe(targetSocket);
-    targetSocket.pipe(clientSocket);
+const server = http.createServer((req, res) => {
+  const options = {
+    hostname: TARGET_HOST,
+    port: TARGET_PORT,
+    path: req.url,
+    method: req.method,
+    headers: {
+      ...req.headers,
+      host: TARGET_HOST
+    }
+  };
+
+  const proxyReq = http.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res, { end: true });
   });
 
-  clientSocket.on('error', () => targetSocket.destroy());
-  targetSocket.on('error', () => clientSocket.destroy());
+  proxyReq.on('error', (err) => {
+    res.writeHead(502, { 'Content-Type': 'text/plain' });
+    res.end('Proxy Error');
+  });
+
+  req.pipe(proxyReq, { end: true });
 });
 
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`TCP Relay running on port ${PORT}`);
+  console.log(`Server active on port ${PORT}`);
 });
